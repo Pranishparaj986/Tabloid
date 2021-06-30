e7647945205313e742ff7ef42d2f2960d42cd25d
Pranishparaj986 <60025181+Pranishparaj986@users.noreply.github.com>
2021-06-30 14:34:49 +0545

    Updated

--- App.js
+++ App.js
@@ -1,37 +1,156 @@
-import 'react-native-gesture-handler'
-import React from 'react'
-import {NavigationContainer} from '@react-navigation/native'
-import {createStackNavigator} from '@react-navigation/stack'
-import choose from './choose'
-import splash from './splash'
-import spaceview from './webviews/spaceview'
-import scienceview from './webviews/scienceview'
-import gamingview from './webviews/gamingview'
-import sportsview from './webviews/sportsview'
-import businessview from './webviews/businessview'
-import booksview from './webviews/booksview'
-import politicsview from './webviews/politicsview'
-import computerview from './webviews/computerview'
+import "react-native-gesture-handler";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import choose from "./choose";
+import splash from "./splash";
+import spaceview from "./webviews/spaceview";
+import scienceview from "./webviews/scienceview";
+import gamingview from "./webviews/gamingview";
+import sportsview from "./webviews/sportsview";
+import businessview from "./webviews/businessview";
+import booksview from "./webviews/booksview";
+import politicsview from "./webviews/politicsview";
+import computerview from "./webviews/computerview";
 
+const Stack = createStackNavigator();
 
-const Stack=createStackNavigator()
-
-function App(){
-    return (
+function App() {
+  return (
     <NavigationContainer>
-     <Stack.Navigator initialRouteName="splash">
-     <Stack.Screen name="splash" component={splash} options={{headerShown:false}}/>
-    <Stack.Screen name="choose" component={choose} options={{headerShown:false}}/>
-    <Stack.Screen name="spaceview" component={spaceview} options={{title:" "}}/>
-    <Stack.Screen name="scienceview" component={scienceview} options={{title:" "}}/>
-    <Stack.Screen name="politicsview" component={politicsview} options={{title:" "}}/>
-    <Stack.Screen name="computerview" component={computerview} options={{title:" "}}/>
-    <Stack.Screen name="gamingview" component={gamingview} options={{title:" "}}/>
-    <Stack.Screen name="businessview" component={businessview} options={{title:" "}}/>
-    <Stack.Screen name="sportsview" component={sportsview} options={{title:" "}}/>
-    <Stack.Screen name="booksview" component={booksview} options={{title:" "}}/>
-    </Stack.Navigator>
+      <Stack.Navigator initialRouteName="splash">
+        <Stack.Screen
+          name="splash"
+          component={splash}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="choose"
+          component={choose}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="spaceview"
+          component={spaceview}
+          options={{
+            title: "SPACE TECHNOLOGY",
+            headerStyle: {
+              borderBottomWidth: 0,
+              backgroundColor: "#3AA2CB",
+            },
+            headerTintColor: "white",
+            headerTitleStyle: {
+              alignContent: "center",
+            },
+          }}
+        />
+        <Stack.Screen
+          name="scienceview"
+          component={scienceview}
+          options={{
+            title: "SCIENCE",
+            headerStyle: {
+              borderBottomWidth: 0,
+              backgroundColor: "#3AA2CB",
+            },
+            headerTintColor: "white",
+            headerTitleStyle: {
+              alignContent: "center",
+            },
+          }}
+        />
+        <Stack.Screen
+          name="politicsview"
+          component={politicsview}
+          options={{
+            title: "POLITICS",
+            headerStyle: {
+              borderBottomWidth: 0,
+              backgroundColor: "#3AA2CB",
+            },
+            headerTintColor: "white",
+            headerTitleStyle: {
+              alignContent: "center",
+            },
+          }}
+        />
+        <Stack.Screen
+          name="computerview"
+          component={computerview}
+          options={{
+            title: "TECH",
+            headerStyle: {
+              borderBottomWidth: 0,
+              backgroundColor: "#3AA2CB",
+            },
+            headerTintColor: "white",
+            headerTitleStyle: {
+              alignContent: "center",
+            },
+          }}
+        />
+        <Stack.Screen
+          name="gamingview"
+          component={gamingview}
+          options={{
+            title: "GAMING",
+            headerStyle: {
+              borderBottomWidth: 0,
+              backgroundColor: "#3AA2CB",
+            },
+            headerTintColor: "white",
+            headerTitleStyle: {
+              alignContent: "center",
+            },
+          }}
+        />
+        <Stack.Screen
+          name="businessview"
+          component={businessview}
+          options={{
+            title: "BUSINESS",
+            headerStyle: {
+              borderBottomWidth: 0,
+              backgroundColor: "#3AA2CB",
+            },
+            headerTintColor: "white",
+            headerTitleStyle: {
+              alignContent: "center",
+            },
+          }}
+        />
+        <Stack.Screen
+          name="sportsview"
+          component={sportsview}
+          options={{
+            title: "SPORTS",
+            headerStyle: {
+              borderBottomWidth: 0,
+              backgroundColor: "#3AA2CB",
+            },
+            headerTintColor: "white",
+            headerTitleStyle: {
+              alignContent: "center",
+            },
+          }}
+        />
+        <Stack.Screen
+          name="booksview"
+          component={booksview}
+          options={{
+            title: "BOOKS & LITERATURE",
+            headerStyle: {
+              borderBottomWidth: 0,
+              backgroundColor: "#3AA2CB",
+            },
+            headerTintColor: "white",
+            headerTitleStyle: {
+              alignContent: "center",
+            },
+          }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
-    )
-  }
-export default App
+  );
+}
+export default App;
